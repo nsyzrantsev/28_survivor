1eb4e4ebaf67541ada9ff607afc3c2ffc9c81616
Nick <nicksyz13@gmail.com>
2020-08-16 19:13:37 +0300

    Initial commit

---
+++ WordSearch.py
@@ -0,0 +1,62 @@
+def StringSeparator(string):
+    line = ''
+    separated_str = []
+    for i, value in enumerate(string):
+        if i == len(string) - 1:
+            line += value
+            separated_str.append(line)
+        else:
+            if len(line) != 0 and value == ' ':
+                separated_str.append(line)
+                separated_str.append(value)
+                line = ''
+            elif value == ' ':
+                separated_str.append(value)
+            else:
+                line += value
+    return separated_str
+
+def WordSeparator(l, string):
+    lst = StringSeparator(string)
+    print(lst)
+    sep_lst = list()
+    line = ''
+    start = 0
+    
+    for i, value in enumerate(lst):
+        if i == len(lst)-1:
+            sep_lst.append(line+value)
+        if len(value) > l:
+            sep_lst.append(value[0:l])
+            line = value[l:]
+        elif len(line + value) <= l:
+            line += value
+        else:
+            sep_lst.append(line)
+            line = value
+            if i == len(lst)-1:
+                sep_lst.append(line)
+
+    return sep_lst
+            
+
+def WordSearch(l, string, subs):
+    sep_lst = WordSeparator(l, string)
+    print(sep_lst)
+    result = list()
+    for e, value in enumerate(sep_lst):
+        word = ''
+        count = 0
+        for i, letter in enumerate(value):
+            if len(word) != 0 and (letter == ' ' or i == len(value)-1):
+                if word == subs:
+                    count += 1
+                word = ''
+            elif letter != ' ':
+                word += letter
+        if count:
+            for i in range(count):
+                result.append(1)
+        else:
+            result.append(0)
+    return result
