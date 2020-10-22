8cb19ba085fdbe52282c4fa5f501a1a54cdec51d
Nick <nicksyz13@gmail.com>
2020-10-22 21:00:53 +0300

    Some changes of the word alignment

--- WordSearch1.py
+++ WordSearch1.py
@@ -7,33 +7,36 @@ def WordsCutter(a, l):
         while len(v) > l:
             lst_words.append(v[0:l])
             v = v[l:]
-        if len(v) <= l:
-            if len(v) < l:
-                v += ' '
-            lst_words.append(v)
+        lst_words.append(v+' ')
     return lst_words
 
 def TextAlignment(words, l):
-    line = words[0]
-    lst = words[1:]
     result = list()
-    if len(words) == 1 and len(line) <= l:
-        result.append(line)
+    line = words[0]
+    if len(words) == 1 and len(line[:-1]) <= l:
+        result.append(line[:-1])
         return result
+    lst = words[1:]
     for i, e in enumerate(lst):
-        if len(line+e) <= l:
+        if len(line+e[:-1]) <= l:
             line += e
             continue
         else:
+            if line[-1] == ' ':
+                line = line[:-1]
             result.append(line)
             line = e
             if i == len(lst)-1:
+                if line[-1] == ' ':
+                    line = line[:-1]
                 result.append(line)
     return result
 
 def WordSearch(length, s, subs):
     formated = WordsCutter(s, length)
+    print(formated)
     text = TextAlignment(formated, length)
+    print(text)
     result = [0]*len(text)
     for i, string in enumerate(text):    
         if subs in string.split():
