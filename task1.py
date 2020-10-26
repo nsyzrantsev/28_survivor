da2c194ccb43d819392686030c0d9f1815a98838
Nick <nicksyz13@gmail.com>
2020-10-26 22:07:35 +0300

    some changes

--- task1.py
+++
@@ -1,9 +0,0 @@
-def squirrel(N):
-    if N < 0:
-        return 'Integer is not positive!'
-    factorial = 1
-    while N > 0:
-        factorial *= N
-        N -= 1
-    first_num = int(str(factorial)[0])
-    return first_num
