87c7ef4568b5e39a565170a6e378a4b7f75336af
Nick <nicksyz13@gmail.com>
2020-10-26 20:23:48 +0300

    some changes

--- BigMinus.py
+++ BigMinus.py
@@ -10,6 +10,7 @@ def BigMinus(s1, s2):
         s1, s2 = s2, s1
     zeros = '0'*(long-short)
     s2 = zeros+s2
+    count_zeros = 0
     for i in range(long):
         num = int(s1[long-1-i]) - int(s2[long-1-i]) + rem
         if num < 0:
@@ -17,6 +18,10 @@ def BigMinus(s1, s2):
             num *= -1
         else:
             rem = 0
+        if num == 0:
+            count_zeros += 1
         sub += str(num)
     sub = sub[::-1]
+    if count_zeros == long:
+        return 0
     return sub
