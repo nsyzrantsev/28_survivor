0f5512e295ce5332dbcb2d511bd506bdb052cbb6
Nick <nicksyz13@gmail.com>
2020-10-31 20:59:10 +0300

    inital commit

--- Unmanned.py
+++ Unmanned.py
@@ -1,20 +1,29 @@
 # https://skillsmart.ru/algo/lvl1/a9d8.html
 
+def wait_time(time, wait, light):
+    wait = light[1] - (time+wait)%(light[1]+light[2])
+    return wait
+
+def check_position(light, L):
+    if light[0] > L:
+        return False
+    return True
+
 def Unmanned(L, N, track):
     time = 0
-    old_light = 0
-    for i in track:
-        new_light = i[0]
-        if new_light > L:
+    error_count = 0
+    last_light = 0
+    all_wait = 0
+    for light in track:
+        if not check_position(light, L):
+            error_count += 1
             continue
-        red = i[1]
-        green = i[2]
-        time += new_light - old_light
-        if red > new_light:
-            time += red - new_light
-        old_light = new_light
-    if track[-1][0] < L:
-        time += L - track[-1][0]
-    if time == 0:
-        time = L
-    return time
+        time += light[0] - last_light
+        wait_t = wait_time(time, all_wait, light)
+        if wait_t > 0:
+            all_wait += wait_t
+        last_light = light[0]
+    if error_count == N:
+        return L
+    time += L - track[-1][0]
+    return time + all_wait
