82bbc44adb80a3cb4c370e397e5116ecfb46ed65
Nick <nicksyz13@gmail.com>
2020-11-08 14:29:04 +0300

    initial commit1

--- MaximumDiscount.py
+++ MaximumDiscount.py
@@ -3,13 +3,11 @@
 def MaximumDiscount(N, price):
     res = ''
     price.sort(reverse=True)
-    print(price)
     method1 = sum(price[-1:-int(N/3)])
     method2 = 0
     count = 2
     for i in range(int(N/3)):
         method2 += price[count]
-        print(method2)
         count += 3
     if method1 < method2:
         return method2
