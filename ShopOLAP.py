a3217fbbab1c59f325d88a91833fb84f1a0cb6a5
Nick <nicksyz13@gmail.com>
2020-12-05 19:41:03 +0300

    initial commit

--- ShopOLAP.py
+++ ShopOLAP.py
@@ -1,25 +1,32 @@
 # https://skillsmart.ru/algo/lvl1/b519.html
 
-def CatalogSort(catalog):
-    result = list()
-    for k, v in catalog.items():
-        line = f'{k} {v}'
-        result.append(line)
-    result = sorted(result)
-    return result
-
 def CatalogMaker(items):
     catalog = dict()
-    for i, line in enumerate(items):
-        name = line.split()[0]
-        num = int(line.split()[1])
-        if name in catalog:
-            catalog[name] += num
+    for line in items:
+        data = line.split()
+        product = data[0]
+        count = data[1]
+        if count not in catalog:
+            catalog[count] = [product]
         else:
-            catalog[name] = num
-    return catalog
+            catalog[count].append(product)
+        catalog[count].sort()
+    return catalog    
+
+def Output(catalog):
+    result = list()
+    keys_list = list(catalog.keys())
+    keys_list.sort(reverse=True)
+    for count in keys_list:
+        for product in catalog[count]:
+            line = f'{product} {count}'
+            result.append(line)
+    return result
 
 def ShopOLAP(N, items):
     catalog = CatalogMaker(items)
-    sorted_catalog = CatalogSort(catalog)
-    return sorted_catalog
+    return Output(catalog)
+
+# items = ['платье1 6', 'сумка128 4', 'сумка23 2', 'сумка32 2']
+# items1 = ["dress1 5", "handbug32 3", "dress2 1", "handbug23 2", "handbug128 4"]
+# print(ShopOLAP(5, items1))
