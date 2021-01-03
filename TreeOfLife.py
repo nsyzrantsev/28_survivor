ddd800a6e3377da08c8494a6927db71d03836bd6
Nick <nicksyz13@gmail.com>
2021-01-03 22:10:35 +0300

    some changes

--- TreeOfLife.py
+++ TreeOfLife.py
@@ -1,45 +1,48 @@
 def ListConv(tree, toList):
     mtx = list()
-    if toList:
-        for i in tree:
+    for i in tree:
+        if toList:
             mtx.append(list(i))
-    else:
-        for i in tree:
+        else:
             mtx.append(''.join([str(j) for j in i]))
     return mtx
+
+def TreeDeath(n, m, H, W, tree):
+    if n >= 0 and n < H and m >= 0 and m < W:
+        if tree[n][m] != '.':
+            if tree[n][m] <= 2:
+                tree[n][m] = '.'
+    return tree
+
+# year of destroy
+def Destroy(tree, H, W):
+    for n in range(H):
+        for m in range(W):
+            if tree[n][m] != '.':
+                if tree[n][m] >= 3:
+                    tree[n][m] = '.'
+                    tree = TreeDeath(n+1, m, H, W, tree)
+                    tree = TreeDeath(n-1, m, H, W, tree)
+                    tree = TreeDeath(n, m+1, H, W, tree)
+                    tree = TreeDeath(n, m-1, H, W, tree)
+    return tree
+
+# year of growth
+def Growth(tree, H, W):
+    for n in range(H):
+        for m in range(W):
+            if tree[n][m] == '.':
+                tree[n][m] = 1
+            elif tree[n][m] == '+':
+                tree[n][m] = 2
+            else:
+                tree[n][m] = tree[n][m] + 1
+    return tree
 
 def TreeOfLife(H, W, N, tree):
     tree = ListConv(tree, True)
     for i in range(1, N+1):
-        for n in range(H):
-            for m in range(W):
-                if tree[n][m] == '.':
-                    tree[n][m] = 1
-                elif tree[n][m] == '+':
-                    tree[n][m] = 2
-                else:
-                    tree[n][m] = tree[n][m] + 1
+        tree = Growth(tree, H, W)
         if i % 2 == 0:
-            for n in range(H):
-                for m in range(W):
-                    if tree[n][m] != '.':
-                        if tree[n][m] >= 3:
-                            tree[n][m] = '.'
-                            if n > 0:
-                                if tree[n-1][m] != '.':
-                                    if tree[n-1][m] <= 2:
-                                        tree[n-1][m] = '.'
-                            if n < H-1:
-                                if tree[n+1][m] != '.':
-                                    if tree[n+1][m] <= 2:
-                                        tree[n+1][m] = '.'
-                            if m > 0:
-                                if tree[n][m-1] != '.':
-                                    if tree[n][m-1] <= 2:
-                                        tree[n][m-1] = '.'
-                            if m < W-1:
-                                if tree[n][m+1] != '.':
-                                    if tree[n][m+1] <= 2:
-                                        tree[n][m+1] = '.'
-    tree = ListConv(tree, False)
-    return tree
+            tree = Destroy(tree, H, W)
+    return ListConv(tree, False)
