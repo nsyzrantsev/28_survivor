3f50d121736e2ba5c27aa0eaa01ec110a87df370
Nick <nicksyz13@gmail.com>
2021-01-07 22:51:12 +0300

    all changes

--- BalancedParentheses.py
+++ BalancedParentheses.py
@@ -1,15 +1,22 @@
-class Ring:
-    def __init__(self):
+# https://skillsmart.ru/algo/lvl1/f70a.html
+class Rings:
+    def __init__(self, N):
+        self.N = N
         self.result = list()
-        
-    def rings_generation(self, ring, N):
-        if not N > 1:
-            self.result.append(ring)
-            return ring
-        self.rings_generation(f'({ring})', N-1)
-        self.rings_generation(f'{ring}()', N-1)
+    
+    def rings_gen(self, ring):
+        rings_lst = ring[:]
+        if ring.count('(') < self.N:
+            for i in range(1, len(ring)+3):
+                rings_lst.insert(i-1, '(')
+                rings_lst.insert(i, ')')
+                ring_line = ''.join(rings_lst)
+                if rings_lst.count('(') == self.N and ring_line not in self.result:
+                    self.result.append(ring_line)
+                self.rings_gen(rings_lst)
+                rings_lst = ring[:]
+        return ' '.join(self.result)
 
 def BalancedParentheses(N):
-    rings = Ring()
-    rings.rings_generation('()', N)
-    return ' '.join(rings.result)
+    ring = Rings(N)
+    return ring.rings_gen(['(', ')'])
