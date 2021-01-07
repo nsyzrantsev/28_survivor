5723cb96fbec711b037c8ebcae44de0b0898d63e
Nick <nicksyz13@gmail.com>
2021-01-07 11:38:12 +0300

    j:k+1

--- TransformTransform.py
+++ TransformTransform.py
@@ -4,11 +4,10 @@ def S(A, N):
     for i in range(N):
         for j in range(N-i):
             k = i + j
-            if len(A[j:k]):
-                B.append(max(A[j:k]))
+            if len(A[j:k+1]):
+                B.append(max(A[j:k+1]))
     return B
 
 def TransformTransform(A, N):
-    L = len(S(A, N))
     B = S(A, N)
-    return sum(S(B, L)) % 2 == 0
+    return not sum(S(B, len(B))) % 2
